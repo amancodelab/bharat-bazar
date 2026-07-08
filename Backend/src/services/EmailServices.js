f1d4a2711deb904ac45b605bce9e5191192642de
amancodelab <studyforbseb12@gmail.com>
2026-07-08 14:41:49 +0530

    Change smt to Verified

--- Backend/src/services/EmailServices.js
+++ Backend/src/services/EmailServices.js
@@ -1,30 +1,39 @@
 require("dotenv").config();
-const nodemailer = require("nodemailer");
-console.log("🔥 EmailServices.js loaded");
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.BREVO_USER,
-    pass: process.env.BREVO_PASS,
-  },
-});
 
 const sendEmail = async (to, subject, text, html) => {
   try {
-
-    const info = await transporter.sendMail({
-      from: `Bharat Bazar <${process.env.BREVO_SENDER}>`,
-      to,
-      subject,
-      text,
-      html,
+    const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "api-key": process.env.BREVO_API_KEY,
+      },
+      body: JSON.stringify({
+        sender: {
+          name: "Bharat Bazar",
+          email: process.env.BREVO_SENDER,
+        },
+        to: [
+          {
+            email: to,
+          },
+        ],
+        subject,
+        htmlContent: html,
+        textContent: text,
+      }),
     });
 
-    console.log("✅ Email sent:", info.messageId);
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(JSON.stringify(data));
+    }
+
+    console.log("✅ Email sent");
+    console.log(data);
   } catch (error) {
-    console.error("❌ Email Error:");
+    console.error("❌ Brevo API Error:");
     console.error(error);
     throw error;
   }
